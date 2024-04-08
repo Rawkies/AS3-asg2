43dd94d73e8fa340c389465123967b71ae92aefd
hasyir <22FTT1502@student.pb.edu.bn>
2024-04-08 21:49:01 +0800

    Update mobile pages and styles

--- src/app/mobile17/mobile17.page.ts
+++ src/app/mobile17/mobile17.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-mobile17',
   templateUrl: './mobile17.page.html',
   styleUrls: ['./mobile17.page.scss'],
 })
 export class Mobile17Page implements OnInit {
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  public actionSheetButtons = [
+    {
+      text: 'Confirm',
+      role: 'Confirm',
+      data: {
+        action: 'Confirm',
+      },
+      handler: () => {
+        this.showRentSuccessAlert();
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  async showRentSuccessAlert() {
+    const alert = await this.alertController.create({
+      message: 'Your item successfully added into cart.',
+      buttons: ['Confirm'],
+    });
+
+    await alert.present();
+  }
 }
--- src/app/mobile18/mobile18.page.ts
+++ src/app/mobile18/mobile18.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-mobile18',
   templateUrl: './mobile18.page.html',
   styleUrls: ['./mobile18.page.scss'],
 })
 export class Mobile18Page implements OnInit {
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  public actionSheetButtons = [
+    {
+      text: 'Confirm',
+      role: 'Confirm',
+      data: {
+        action: 'Confirm',
+      },
+      handler: () => {
+        this.showRentSuccessAlert();
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  async showRentSuccessAlert() {
+    const alert = await this.alertController.create({
+      message: 'Your item successfully added into cart.',
+      buttons: ['Confirm'],
+    });
+
+    await alert.present();
+  }
 }
