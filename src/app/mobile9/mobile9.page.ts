f7f0319a8e39ea5818cfe262d1e48ff8dd681047
hasyir <22FTT1502@student.pb.edu.bn>
2024-04-08 20:37:44 +0800

    Update mobile pages and styles

--- src/app/mobile9/mobile9.page.ts
+++ src/app/mobile9/mobile9.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-mobile9',
   templateUrl: './mobile9.page.html',
   styleUrls: ['./mobile9.page.scss'],
 })
 export class Mobile9Page implements OnInit {
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  public actionSheetButtons = [
+    {
+      text: 'Confirm',
+      role: 'Confirm',
+      data: {
+        action: 'Confirm',
+      },
+      handler: () => {
+        this.showRentSuccessAlert();
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  async showRentSuccessAlert() {
+    const alert = await this.alertController.create({
+      message: 'Your item successfully added into cart.',
+      buttons: ['Confirm'],
+    });
+
+    await alert.present();
+  }
 }
